bdfe0e337fbc51bbd77a0b4c1504d8ce76646198
chetan22 <ck.chetan20@gmail.com>
2020-02-11 00:49:02 +0530

    Testing login page by Kriti

--- transfers/views/__init__.py
+++ transfers/views/__init__.py
@@ -1,2 +1,4 @@
 from .redirect_views import * # NOQA
 from .student_views import * # NOQA
+from .hod_views import * #NOQA
+from .supervisor_views import * # NOQA
--- transfers/views/redirect_views.py
+++ transfers/views/redirect_views.py
@@ -3,4 +3,10 @@ from django.shortcuts import redirect
 from transfers.constants import UserType
 
 def login_redirect_view(request):
-    pass
+    user_type = request.user.userprofile.user_type
+    if user_type == UserType.STUDENT.value:
+        return redirect('/TMS/student/dashboard/')
+    elif user_type == UserType.SUPERVISOR.value:
+        return redirect('/TMS/hod/home/')
+    elif user_type == UserType.HOD.value:
+        return redirect('/TMS/supervisor/home/')
