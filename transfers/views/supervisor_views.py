8ff939ea00460caef6e246a1f781d183d47fa391
chetan22 <ck.chetan20@gmail.com>
2020-02-17 22:08:09 +0530

    Bug fix: campus value was sent instead of name

--- transfers/views/supervisor_views.py
+++ transfers/views/supervisor_views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import generic
 
-from transfers.constants import UserType
+from transfers.constants import UserType, CampusType
 from transfers.models import PS2TSTransfer, UserProfile
 
 
@@ -17,6 +17,8 @@ def get_supervisor_data(request):
     response = {}
     try:
         current_user = request.user
+        current_user_alias = 'Student Supervisor'
+        campus_alias = CampusType._member_names_[current_user.userprofile.campus]
         # applications where approval is pending
         pending_applications_qs = PS2TSTransfer.objects.filter(
             hod_email = current_user.email,
@@ -40,7 +42,14 @@ def get_supervisor_data(request):
         response['error'] = False
         response['message'] = 'success'
         response['data'] = {}
-        response['data']['student_pending_attributes'] = [
+        response['data']['user'] = {
+            'username': current_user.username,
+            'designation': current_user_alias,
+            'campus': campus_alias,
+            'email': current_user.email
+
+        }
+        attributes_for_display = [
             {'display':'Student First Name','prop':'applicant__user__first_name'},
             {'display':'Student Last Name','prop':'applicant__user__last_name'},
             {'display':'CGPA','prop':'cgpa'},
@@ -50,10 +59,18 @@ def get_supervisor_data(request):
             {'display':'Organisation','prop':'name_of_org'},
             {'display':'Expected Deliverables','prop':'expected_deliverables'},
         ]
+        response['data']['student_pending_attributes'] = attributes_for_display
+        response['data']['student_approved_attributes'] = attributes_for_display
         response['data']['data_pending'] = [a for a in pending_applications_list]
         response['data']['data_approved']= [a for a in approved_applications_list]
     except Exception as e:
-        response['data'] = {}
+        response['data'] = {
+            'username': current_user.username,
+            'designation': current_user_alias,
+            'campus': campus_alias,
+            'email': current_user.email
+
+        }
         response['error'] = True
         response['message'] = 'error'
     return JsonResponse(response, safe=False)
