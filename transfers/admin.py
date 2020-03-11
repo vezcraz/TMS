d0d3dc1fdb9061a16142a5d5704613d91c1fc0ce
Nrupesh Surya <nrupeshsurya@gmail.com>
2020-03-11 06:33:50 +0530

    admin filters

--- transfers/admin.py
+++ transfers/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from import_export.admin import ImportExportModelAdmin
 
 from .models import *
 
@@ -20,9 +21,18 @@ class CustomUserAdmin(UserAdmin):
 			return []
 		return super(CustomUserAdmin, self).get_inline_instances(request, obj)
 
+class PS2TSTransferadmin(ImportExportModelAdmin):
+    	list_display = ('applicant','supervisor_email','hod_email',)
+    	search_fields = ('applicant__user__username','supervisor_email','hod_email',)
+
+class TS2PSTransferadmin(ImportExportModelAdmin):
+    	list_display = ('applicant','hod_email',)
+    	search_fields = ('applicant__user__username','hod_email',)
+	
+
 
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
-admin.site.register(PS2TSTransfer)
-admin.site.register(TS2PSTransfer)
+admin.site.register(PS2TSTransfer,PS2TSTransferadmin)
+admin.site.register(TS2PSTransfer,TS2PSTransferadmin)
 admin.site.register(DeadlineModel)
