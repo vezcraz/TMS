ef94b152d23db6e9716575fb4aa24217ed1434a2
chetan22 <ck.chetan20@gmail.com>
2020-03-22 15:24:55 +0530

    typo in ALLOWED_HOSTS settings

--- TMS/settings.py
+++ TMS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7(^b1g(c5260n(a@pvfk85hg54%t7juq3sv0k0h9ctycy*g1)-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '13.126.92.97', 'bits-pilani.in', 'www.bits-pilani.in', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['localhost', '13.126.92.97', 'bits-pilani.in', 'www.bits-pilani.in', '127.0.0.1']
 
 
 # Application definition
